07d27fc7d483a9207d6efea575718b3747b65eef
ECA08.谭渊 <tanyuan@Dev.chngalaxy.com>
2018-02-09 15:35:36 +0800

    change HRM XOR to function instead of process

--- HRM_XOR.py
+++ HRM_XOR.py
@@ -1,16 +1,22 @@
 # input a list of tuples
 # output if each tuple's sign are the same
 
-list = [(1,2), (-1,-2), (1,-9), (0,-1), (1, 0), (0, 0)]
-output = []
-for tup in list:
-    # determine if the sign of each number is the same
-    if (tup[0] * tup[1] > 0):
-        output.append(0)
-    elif (tup[0] * tup[1] == 0):
-        output.append(0)
-    else:
-        output.append(1)
+def XOR(list=[()]):
+    output=[]
+    for tup in list:
+        if tup[0] * tup[1] > 0:
+            output.append(0)
+        elif tup[0] * tup[1] == 0:
+            output.append(0)
+        else:
+            output.append(1)
+    return output
+
+
+
+
+input = [(1,2), (-1,-2), (1,-9), (0,-1), (1, 0), (0, 0)]
+output = XOR(input)
 
 print("Expected output: ", [0, 0, 1, 0, 0, 0])
 print("XOR list output ", output)
