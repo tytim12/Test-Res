d28cd3e5ef8da9b033c3769cb40fc1f9becdf91e
ECA08.谭渊 <tanyuan@Dev.chngalaxy.com>
2018-02-28 09:54:55 +0800

    heap sort submit

---
+++ heap_sort.py
@@ -0,0 +1,17 @@
+from heapq import heappush, heappop
+
+
+def heap_sort(iterable):
+    h = []
+    for value in iterable:
+        heappush(h, value)
+    return [heappop(h) for i in range(len(h))]
+
+
+def main():
+    L = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0]
+    print (heap_sort(L))
+
+
+if __name__ == "__main__":
+    main()
